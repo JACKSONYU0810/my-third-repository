e5e8d7c819ccb5df162cb073244276893f07556b
yuhuajie <yuhuajie0810@gmail.com>
2022-05-30 17:22:07 +0800

    添加了id

---
+++ src/com/jackson/domain/Person.java
@@ -0,0 +1,14 @@
+package com.jackson.domain;
+
+/**
+ * ClassName: Person
+ * Package: com.jackson
+ * Description:
+ *
+ * @date: 5/30/2022 5:06 PM
+ * @author: Jackson Yae
+ */
+public class Person {
+    private Integer id;
+
+}
